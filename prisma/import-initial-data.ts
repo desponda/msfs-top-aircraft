dc186199cdd0847efcce5d9e9a69359a34389034
Daniel Esponda <daniel.esponda@datadoghq.com>
2025-05-12 03:12:06 +0000

    chore: update import-initial-data.ts to change data directory path from 'prisma/data' to 'data'

--- prisma/import-initial-data.ts
+++ prisma/import-initial-data.ts
@@ -5,7 +5,7 @@ import path from 'path';
 const prisma = new PrismaClient();
 
 async function importData() {
-  const exportDir = path.join(process.cwd(), 'prisma/data');
+  const exportDir = path.join(process.cwd(), 'data');
   const aircraft = JSON.parse(fs.readFileSync(path.join(exportDir, 'aircraft.json'), 'utf-8'));
   const reports = JSON.parse(fs.readFileSync(path.join(exportDir, 'reports.json'), 'utf-8'));
   const aircraftVotes = JSON.parse(fs.readFileSync(path.join(exportDir, 'aircraftVotes.json'), 'utf-8'));
